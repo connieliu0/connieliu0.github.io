a8f53b6b69aafba24d596a2e8609dfdb17097e68
cl2264 <cl2264@cornell.edu>
2020-01-01 01:20:15 -0500

    Updates

---
+++ precache-manifest.89f59f0dce7fdfca95ffbcd1c55b3a5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc64c9bfa1c6c05916daac50c95e72b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b6190608a40059212e0",
+    "url": "/static/css/2.4c43d4f9.chunk.css"
+  },
+  {
+    "revision": "e7b6340dc49d7cf9f955",
+    "url": "/static/css/main.5cef4c54.chunk.css"
+  },
+  {
+    "revision": "0b6190608a40059212e0",
+    "url": "/static/js/2.148194e5.chunk.js"
+  },
+  {
+    "revision": "fec8010374a5996c79e5975a9066a6ad",
+    "url": "/static/js/2.148194e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7b6340dc49d7cf9f955",
+    "url": "/static/js/main.29902464.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  }
+]);
