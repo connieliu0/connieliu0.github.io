a77ea7d88e1b52d3de68158e0999f5ff2873c2b4
Connie <cl2264@cornell.edu>
2022-08-25 15:32:12 -0400

    Updates

--- precache-manifest.75c43415e4539a28eaed3356e9b64029.js
+++ precache-manifest.75c43415e4539a28eaed3356e9b64029.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64fb0a23dbb2f886126ab4f666cf1cdf",
+    "revision": "db007c3445f905105430560baacca8d2",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.4c43d4f9.chunk.css"
   },
   {
-    "revision": "168581470a53b5ba63ec",
+    "revision": "26e25fda87b1f2a48ddd",
     "url": "/static/css/main.52a1e0c9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f2dfb001.chunk.js.LICENSE"
   },
   {
-    "revision": "168581470a53b5ba63ec",
-    "url": "/static/js/main.4927cc67.chunk.js"
+    "revision": "26e25fda87b1f2a48ddd",
+    "url": "/static/js/main.3080118d.chunk.js"
   },
   {
     "revision": "00ccf1beb4d435cfbe60",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50bb41bf17824af99fb36ff93c40ae77.js"
+  "/precache-manifest.75c43415e4539a28eaed3356e9b64029.js"
 );
 
 self.addEventListener('message', (event) => {
